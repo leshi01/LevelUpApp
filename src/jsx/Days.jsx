ca582c007fd21f263754e85e6ad2a59a26af0372
shkelzen <lenilesi28@gmail.com>
2024-11-22 15:53:29 +0200

    refactor Background and index styles; enhance Days component layout and functionality; update Timetable component structure

--- src/jsx/Days.jsx
+++ src/jsx/Days.jsx
@@ -15,8 +15,18 @@ function CustomTabPanel(props) {
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
       {...other}
+      style={{
+        height: '100%',
+        overflowY: 'auto',
+        msOverflowStyle: 'none', // Hide scrollbar in IE and Edge
+        scrollbarWidth: 'none', // Hide scrollbar in Firefox
+      }}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {value === index && (
+        <Box sx={{ p: 3, '&::-webkit-scrollbar': { display: 'none' } }}>
+          {children}
+        </Box>
+      )}
     </div>
   );
 }
@@ -42,42 +52,60 @@ export default function Days() {
   };
 
   return (
-    <Box sx={{ width: '100%'}}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
-            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="Monday" {...a11yProps(0)} />
-                <Tab label="Tusday" {...a11yProps(1)} />
-                <Tab label="Wendesday" {...a11yProps(2)} />
-                <Tab label="Thursday" {...a11yProps(3)} />
-                <Tab label="Friday" {...a11yProps(4)} />
-                <Tab label="Saturday" {...a11yProps(5)} />
-            </Tabs>
-         </Box>
-        
-      <Box sx={{  display: 'flex', justifyContent: 'center', color:'black' }}>
-        <CustomTabPanel value={value} index={0}>
-            <Timetable/>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-        <Timetable/>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', overflow: 'hidden', position: 'static' }}>
+      <Box sx={{ width: '80%', height: '100%', alignSelf: 'center', overflow: 'hidden' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center', position: 'sticky', top: 0, zIndex: 1 }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Monday" {...a11yProps(0)} />
+            <Tab label="Tuesday" {...a11yProps(1)} />
+            <Tab label="Wednesday" {...a11yProps(2)} />
+            <Tab label="Thursday" {...a11yProps(3)} />
+            <Tab label="Friday" {...a11yProps(4)} />
+            <Tab label="Saturday" {...a11yProps(5)} />
+          </Tabs>
+        </Box>
 
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={2}>
-        <Timetable/>
-
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={3}>
-        <Timetable/>
-
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={4}>
-        <Timetable/>
-
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={5}>
-        <Timetable/>
-
-        </CustomTabPanel>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'black', width: '100%', overflow: 'hidden' }}>
+          <CustomTabPanel value={value} index={0}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Timetable time="09:00 - 10:00" />
+              <Timetable time="10:00 - 11:00" />
+              <Timetable time="11:00 - 12:00" />
+              <Timetable time="12:00 - 13:00" />
+              <Timetable time="13:00 - 14:00" />
+              <Timetable time="14:00 - 15:00" />
+              <Timetable time="15:00 - 16:00" />
+              <Timetable time="13:00 - 14:00" />
+              <Timetable time="14:00 - 15:00" />
+              <Timetable time="15:00 - 16:00" />
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Timetable />
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Timetable />
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={3}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Timetable />
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={4}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Timetable />
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={5}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Timetable />
+            </Box>
+          </CustomTabPanel>
+        </Box>
       </Box>
     </Box>
   );
